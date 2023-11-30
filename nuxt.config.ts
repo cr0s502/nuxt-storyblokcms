0b462d0a5a5757ead0accee8f4930c909a3c1aaa
CrossDev <joancruz0502@hotmail.com>
2023-11-30 14:49:10 -0400

    feat: setup css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['@/assets/main.css'],
   modules: [['@storyblok/nuxt', {
     accessToken: "IabpqsFK2W9Ye8W0UyYMEQtt"
   }]
